3f2dff0fbaca3b9e16eecf80409a560250898109
dmka0218 <dmitrii.kazanin@netcracker.com>
2025-04-11 21:37:47 +0700

    bugfix/fatal2lvl

--- controllers/fluentbit-forwarder-aggregator/aggregator.configmap/conf.d/lua_scripts/update_level_syslog.lua
+++ controllers/fluentbit-forwarder-aggregator/aggregator.configmap/conf.d/lua_scripts/update_level_syslog.lua
@@ -24,7 +24,7 @@ function update_level(tag, timestamp, record)
       return 2, timestamp, record
     end
     if (record["level"] == "fatal") then
-      record["level"] = "emerg"
+      record["level"] = "crit"
       return 2, timestamp, record
     end
     if (record["level"] == "critical") then

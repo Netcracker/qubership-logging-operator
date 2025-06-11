d968f30c37e7acd6f05eba722756842cac35fddd
Dmitrii Kazanin <dmitrii.kazanin@netcracker.com>
2025-06-11 17:12:50 +0700

    fix: updated log level mapping (#92)
    * fix: updated log level mapping
    
    * fix: updated log level mapping

--- scripts/lua-tests/test-update-level.lua
+++ scripts/lua-tests/test-update-level.lua
@@ -77,38 +77,50 @@ local test_strings = {
 -- input: https://docs.fluentbit.io/manual/pipeline/filters/lua#function-arguments
 -- output: https://docs.fluentbit.io/manual/pipeline/filters/lua#return-values
 function update_level(tag, timestamp, record)
+  record["source_level"] = record["level"]
   if (record["level"] ~= nil) then
-    record["level"] = string.lower(record["level"])
-
-    -- return 2 everywhere inside the if block,
-    -- that means the original timestamp is not modified and the record has been modified
-    if (record["level"] == "error") then
-        record["level"] = "err"
-        return 2, timestamp, record
-    end
-    if (record["level"] == "warn") then
-        record["level"] = "warning"
-        return 2, timestamp, record
-    end
-    if (record["level"] == "trace") then
-      record["level"] = "debug"
-      return 2, timestamp, record
-    end
-    if (record["level"] == "fatal") then
+    record["level"] = string.lower(record["level"]):gsub("^%s*(.-)%s*$", "%1")
+    local first_ch = string.sub(record["level"], 1, 1)
+    if first_ch == '0' then
+      record["level"] = "emerg"
+    -- a = alert, f = fatal, s = severe
+    elseif first_ch == '1' or first_ch == 'a' or first_ch == 'f' or first_ch == 's' then
       record["level"] = "alert"
-      return 2, timestamp, record
-    end
-    if (record["level"] == "critical") then
+    -- c = crit
+    elseif first_ch == '2' or first_ch == 'c' then
       record["level"] = "crit"
-      return 2, timestamp, record
+    elseif first_ch == '3' then
+      record["level"] = "err"
+    -- w = warning
+    elseif first_ch == '4' or first_ch == 'w' then
+      record["level"] = "warning"
+    -- n = notice
+    elseif first_ch == '5' or first_ch == 'n' then
+      record["level"] = "notice"
+    -- i = info
+    elseif first_ch == '6' or first_ch == 'i' then
+      record["level"] = "info"
+    -- d = debug, t = trace
+    elseif first_ch == '7' or first_ch == 'd' or first_ch == 't' then
+      record["level"] = "debug"
+    -- e, er = err, e(~=r) = emerg
+    elseif first_ch == 'e' then
+      if string.len(record["level"]) >=2 and string.sub(record["level"], 2, 2) ~= 'r' then
+        record["level"] = "emerg"
+      else
+        record["level"] = "err"
+      end
+    else
+      record["level_unknown"] = "true"
+      record["level"] = "info"
     end
-
-    -- return 0, that the record will not be modified
-    return 0, timestamp, record
+  else
+    record["level_unknown"] = "true"
+    record["level"] = "info"
   end
 
-  -- return 0, that the record will not be modified
-  return 0, timestamp, record
+  -- return 2, that means the original timestamp is not modified and the record has been modified
+  return 2, timestamp, record
 end
 
 -- test functions

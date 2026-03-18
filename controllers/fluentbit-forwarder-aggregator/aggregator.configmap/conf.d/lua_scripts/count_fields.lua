9ae2110f910987f104591f956c34df65c08a2c6b
Beauline <valentina.feshina.qubership@gmail.com>
2026-03-18 17:48:53 +0300

    chore: add detected_level and protection from nil comparison

--- controllers/fluentbit-forwarder-aggregator/aggregator.configmap/conf.d/lua_scripts/count_fields.lua
+++ controllers/fluentbit-forwarder-aggregator/aggregator.configmap/conf.d/lua_scripts/count_fields.lua
@@ -17,14 +17,25 @@ function second_count_fields(tag, timestamp, record)
         for _ in pairs(record) do
             count = count + 1
         end
-        if record["orig_field_count"] ~= nil then
-            count = count - 1 -- Subtracting orig_field_count
+        if record["logfmt_candidate"] ~= nil then
+            count = count - 1 -- Subtracting logfmt_candidate
         end
         if record["field_count"] ~= nil then
-            count = count - 2 -- Subtracting field_count and parse_status
+            count = count - 1 -- Subtracting field_count
         end
-        if (count > record["orig_field_count"]) then
-            record["parse_status"] = "success"
+        if record["parse_status"] == "success" then
+            return 0, timestamp, record
+        elseif record["parse_status"] ~= nil then
+            count = count - 1
+        end
+
+        if record["orig_field_count"] ~= nil then
+            count = count - 1 -- Subtracting orig_field_count
+            if (count > record["orig_field_count"]) then
+                record["parse_status"] = "success"
+            else
+                record["parse_status"] = "failed"
+            end
         else
             record["parse_status"] = "failed"
         end

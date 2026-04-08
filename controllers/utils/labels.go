8448611ddb343b5abc7b717328747d5636f9b6a5
Alexey Karasev <oneasat@gmail.com>
2026-04-08 14:18:53 +0500

    chore: remove duplicated import

--- controllers/utils/labels.go
+++ controllers/utils/labels.go
@@ -3,7 +3,6 @@ package utils
 import (
 	"fmt"
 	"maps"
-	maps0 "maps"
 	"strings"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -43,13 +42,13 @@ func ResourceLabels(name, component string) map[string]string {
 
 // MergeLabels returns a new map with all key-value pairs from the given maps.
 // Later maps override earlier ones on key conflict. Nil maps are skipped.
-func MergeLabels(maps ...map[string]string) map[string]string {
+func MergeLabels(mapsToMerge ...map[string]string) map[string]string {
 	out := make(map[string]string)
-	for _, m := range maps {
+	for _, m := range mapsToMerge {
 		if m == nil {
 			continue
 		}
-		maps0.Copy(out, m)
+		maps.Copy(out, m)
 	}
 	return out
 }

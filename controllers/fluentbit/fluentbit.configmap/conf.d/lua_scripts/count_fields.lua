ef5a79f0175b2dff467072a00ee2c8b8fbc50c2a
Dmitrii Kazanin <dmitrii.kazanin@netcracker.com>
2026-08-26 15:04:20 +0700

    fix: optimized second_count_fields function (#409)

--- controllers/fluentbit/fluentbit.configmap/conf.d/lua_scripts/count_fields.lua
+++ controllers/fluentbit/fluentbit.configmap/conf.d/lua_scripts/count_fields.lua
@@ -2,7 +2,7 @@
 function first_count_fields(tag, timestamp, record)
     local count = 0
     for _ in pairs(record) do
-    count = count + 1
+        count = count + 1
     end
     if record["log_parsed"] ~= nil then
         count = count - 1 -- Subtracting log_parsed
@@ -12,34 +12,24 @@ function first_count_fields(tag, timestamp, record)
 end
 -- Count fields after parsing
 function second_count_fields(tag, timestamp, record)
-    if record["log"] ~= nil then
-        local count = 0
-        for _ in pairs(record) do
+    if record["log"] == nil or record["parse_status"] == "success" then
+        return 0, timestamp, record
+    end
+    local count = 0
+    for k in pairs(record) do
+        if   k ~= "logfmt_candidate"
+         and k ~= "parse_field_count"
+         and k ~= "parse_status"
+         and k ~= "orig_field_count" then
             count = count + 1
         end
-        if record["logfmt_candidate"] ~= nil then
-            count = count - 1 -- Subtracting logfmt_candidate
-        end
-        if record["parse_field_count"] ~= nil then
-            count = count - 1 -- Subtracting parse_field_count
-        end
-        if record["parse_status"] == "success" then
-            return 0, timestamp, record
-        elseif record["parse_status"] ~= nil then
-            count = count - 1
-        end
-
-        if record["orig_field_count"] ~= nil then
-            count = count - 1 -- Subtracting orig_field_count
-            if (count > record["orig_field_count"]) then
-                record["parse_status"] = "success"
-            else
-                record["parse_status"] = "failed"
-            end
-        else
-            record["parse_status"] = "failed"
-        end
-        record["parse_field_count"] = count
-        return 2, timestamp, record
     end
+    local orig_count = record["orig_field_count"]
+    if orig_count ~= nil and count > orig_count then
+        record["parse_status"] = "success"
+    else
+        record["parse_status"] = "failed"
+    end
+    record["parse_field_count"] = count
+    return 2, timestamp, record
 end
